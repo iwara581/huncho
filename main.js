db8a3031b52e4cfa736287814df6c77850d281ab
deadpool06 <80097229+iwara581@users.noreply.github.com>
2021-11-14 12:50:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const h1 = document.querySelector('.banner h1')
+const h5 = document.querySelector('.banner h5')
+const h6 = document.querySelector('.banner h6')
+const ban = document.querySelector('.banner')
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    h1.classList.add('animate__bounceOutUp')
+  }, 2000)
+  
+  setTimeout(() => {
+    h6.classList.add('animate__bounceOutRight')
+  }, 3000)
+  
+  setTimeout(() => {
+    h5.classList.add('animate__bounceOutLeft')
+  }, 4000)
+  
+  setTimeout(() => {
+    ban.classList.add('banOut')
+  }, 5000)
+})
